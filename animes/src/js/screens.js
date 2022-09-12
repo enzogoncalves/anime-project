1a0a5a7e9d480e065fadcc5692e7e4f3f9e6e2b9
Enzo Mateus Gonçalves <enzomateusgonc@gmail.com>
2022-09-12 16:07:22 -0300

    .

---
+++ animes/src/js/screens.js
@@ -0,0 +1,93 @@
+const popularityAnimes = document.querySelector("#popularity");
+
+function createLoadingScreen() {
+  let loadingDiv = document.createElement("div");
+  loadingDiv.classList.add("loading");
+
+  const img = document.createElement("img");
+  img.setAttribute("src", "../assets/icon/spinner.svg");
+
+  const p = document.createElement("p");
+  p.textContent = "carregando...";
+
+  loadingDiv.appendChild(img);
+  loadingDiv.appendChild(p);
+
+  return loadingDiv;
+}
+
+function createErrorScreen() {
+  let errorScreen = document.createElement("div");
+  errorScreen.classList.add("error-screen");
+
+  const img = document.createElement("img");
+  img.setAttribute("src", "../assets/img/undraw_server_down_s-4-lk.svg");
+
+  const p = document.createElement("p");
+  p.textContent = "Não conseguimos conectar ao servidor :(";
+
+  errorScreen.appendChild(img);
+  errorScreen.appendChild(p);
+
+  return errorScreen;
+}
+
+function createNonAnimeFoundScreen() {
+  let NonAnimeFoundScreen = document.createElement("div");
+  NonAnimeFoundScreen.classList.add("nonAnimeFound");
+
+  const a = document.createElement("a");
+  a.setAttribute("href", "#");
+  a.textContent = "Clique aqui para adicionar!";
+
+  const p = document.createElement("p");
+  p.textContent = "Nenhum anime encontrado. ";
+
+  p.appendChild(a);
+
+  NonAnimeFoundScreen.appendChild(p);
+
+  return NonAnimeFoundScreen;
+}
+
+function createAnimeScreen(
+  animeId,
+  animeImg = "",
+  animeTile = "",
+  animeEp = "0"
+) {
+  console.log(animeId);
+  const animeScreen = document.createElement("div");
+  animeScreen.setAttribute("id", animeId);
+  animeScreen.classList.add("anime", "carousel-cell");
+
+  const div = document.createElement("div");
+  div.classList.add("anime-img");
+
+  const animeImgElement = document.createElement("img");
+  animeImgElement.setAttribute("src", animeImg);
+  animeImgElement.setAttribute("alt", "anime image");
+
+  const animeEpElement = document.createElement("span");
+  animeEpElement.classList.add("episodes");
+  animeEpElement.textContent = animeEp + " episodes";
+
+  const animeTitleElement = document.createElement("p");
+  animeTitleElement.classList.add("anime-title");
+  animeTitleElement.textContent = animeTile;
+
+  div.appendChild(animeImgElement);
+  div.appendChild(animeEpElement);
+
+  animeScreen.appendChild(div);
+  animeScreen.appendChild(animeTitleElement);
+
+  popularityAnimes.appendChild(animeScreen);
+}
+
+export {
+  createAnimeScreen,
+  createErrorScreen,
+  createLoadingScreen,
+  createNonAnimeFoundScreen,
+};
--- animes/src/js/script.js
+++ animes/src/js/script.js
@@ -1,103 +1,58 @@
+import {
+  createAnimeScreen,
+  createErrorScreen,
+  createLoadingScreen,
+  createNonAnimeFoundScreen,
+} from "./screens.js";
+
 const w = document.querySelector("#w");
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   w.textContent = window.innerWidth;
-})
-
-const menu_toggle = document.querySelector(".menu_toggle")
-const nav = document.querySelector("nav")
-menu_toggle.addEventListener("click", ({target}) => {
-  target.getAttribute('src') == "../assets/icon/menu.svg" 
-  ? menu_toggle.src = "../assets/icon/clear.svg" 
-  : menu_toggle.src = "../assets/icon/menu.svg"
-
-  nav.classList.contains("active") 
-  ? nav.classList.remove("active")
-  : nav.classList.add("active")
-})
-
-const elem = document.querySelector('.main-carousel');
-const flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
 });
 
+const menu_toggle = document.querySelector(".menu_toggle");
+const nav = document.querySelector("nav");
+menu_toggle.addEventListener("click", ({ target }) => {
+  target.getAttribute("src") == "../assets/icon/menu.svg"
+    ? (menu_toggle.src = "../assets/icon/clear.svg")
+    : (menu_toggle.src = "../assets/icon/menu.svg");
+
+  nav.classList.contains("active")
+    ? nav.classList.remove("active")
+    : nav.classList.add("active");
+});
 
 // --- SCREENS ---
 
-function createLoadingScreen() {
-  let loadingDiv = document.createElement('div')
-  loadingDiv.classList.add("loading")
-
-  const img = document.createElement('img');
-  img.setAttribute("src", "../assets/icon/spinner.svg")
-
-  const p = document.createElement('p');
-  p.textContent = "carregando..."
-
-  loadingDiv.appendChild(img)
-  loadingDiv.appendChild(p)
-
-  return loadingDiv;
+function carousel() {
+  const elem = document.querySelector(".main-carousel");
+  const flkty = new Flickity(elem, {
+    // options
+    cellAlign: "left",
+    contain: true,
+    pageDots: false,
+  });
 }
 
-function createErrorScreen() {
-  let errorScreen = document.createElement('div')
-  errorScreen.classList.add("error-screen")
-
-  const img = document.createElement('img');
-  img.setAttribute("src", "../assets/img/undraw_server_down_s-4-lk.svg")
-
-  const p = document.createElement('p');
-  p.textContent = "Não conseguimos conectar ao servidor :("
-
-  errorScreen.appendChild(img)
-  errorScreen.appendChild(p)
-
-  return errorScreen;
+function getTopAnimes() {
+  axios
+    .get("https://api.jikan.moe/v4/top/anime")
+    .then((res) => res.data.data)
+    .then((data) => {
+      data.forEach((anime) => {
+        createAnimeScreen(
+          anime.mal_id,
+          anime.images.webp.image_url,
+          anime.title,
+          anime.episodes
+        );
+      });
+    })
+    .then(() => {
+      carousel();
+    })
+    .catch((err) => console.log(err));
 }
 
-function createNonAnimeFoundScreen() {
-  let NonAnimeFoundScreen = document.createElement('div')
-  NonAnimeFoundScreen.classList.add("nonAnimeFound")
-
-  const a = document.createElement("a");
-  a.setAttribute("href", "#")
-  a.textContent = "Clique aqui para adicionar!"
-
-  const p = document.createElement('p');
-  p.textContent = "Nenhum anime encontrado. "
-
-  p.appendChild(a)
-
-  NonAnimeFoundScreen.appendChild(p)
-
-  return NonAnimeFoundScreen;
-}
-
-function createAnimeScreen(animeImg = "", animeTile = "", animeEp = "0") {
-  const animeScreen = document.createElement('div')
-  animeScreen.classList.add('anime carousel-cell')
-
-  const div = document.createElement('div')
-  div.classList.add("anime-img")
-
-  const animeImgElement = document.createElement('img')
-  animeImgElement.setAttribute('src', animeImg)
-  animeImgElement.setAttribute('alt', 'anime image')
-
-  const animeEpElement = document.createElement('span')
-  animeEpElement.classList.add("episodes")
-  animeEpElement.textContent = animeEp;
-
-  const animeTitleElement = document.createElement('p')
-  animeTitleElement.classList.add("anime-title")
-  animeTitleElement.textContent = animeTile;
-
-  div.appendChild(animeImgElement)
-  div.appendChild(animeEpElement)
-  return animeScreen;
-}
-
-axios.get("")
+getTopAnimes();
