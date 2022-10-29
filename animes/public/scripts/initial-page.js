91807e8ca8bcc1d2432897e51c4339f3fd405627
enzogoncalves <enzomateusgonc@gmail.com>
2022-10-29 20:01:53 -0300

    Revert "flickity errors"
    This reverts commit 406f88694c8deb89f30c72e40fdee9f3a835d741.

--- animes/public/scripts/initial-page.js
+++ animes/public/scripts/initial-page.js
@@ -4,7 +4,7 @@ import {
 
 import { getAnimes, handleResponse, handleError } from "./api_requests.js";
 
-// import { default as carousel } from "./script.js";
+import { default as carousel } from "./script.js";
 
 function handleData(data, parentEl) {
   data.data.Page.media.forEach((anime) => {
@@ -30,13 +30,7 @@ function showAnimes(sort, parentEl) {
       handleData(data, parentEl);
     })
     .catch(handleError)
-    .finally(createScriptTag());
+    .finally(carousel);
 }
 
 showAnimes("TRENDING_DESC", 1);
-
-function createScriptTag() {
-  let script = document.createElement('script')
-  script.src = '/scripts/flickity.js'
-  document.body.append(script)
-}
--- animes/public/scripts/script.js
+++ animes/public/scripts/script.js
@@ -20,15 +20,15 @@ function openMenu(el) {
     : nav.classList.add("active");
 }
  
-// export default function carousel() {
-//   const elem = document.querySelectorAll(".main-carousel");
-//   elem.forEach((el) => {
-//     const flkty = new Flickity(el, {
-//       // options
-//       cellAlign: "left",
-//       contain: true,
-//       pageDots: false,
-//     });
-//   });
-// }
+export default function carousel() {
+  const elem = document.querySelectorAll(".main-carousel");
+  elem.forEach((el) => {
+    const flkty = new Flickity(el, {
+      // options
+      cellAlign: "left",
+      contain: true,
+      pageDots: false,
+    });
+  });
+}
 
